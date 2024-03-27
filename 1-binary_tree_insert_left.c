c4aade858b2a2e49dc179a0b20ca782a8436cfb5
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-03-27 22:10:56 +0000

    C - Binary trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,11 +21,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
-	parent->left = new;
 	if (parent->left)
 	{
 		new->left = parent->left;
 		parent->left->parent = new;
 	}
+	parent->left = new;
 	return (new);
 }
